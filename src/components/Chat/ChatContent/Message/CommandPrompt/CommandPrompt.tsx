3ffb933f36fb7a2d5705abf8442c8b557d257776
broken-admin <alex@alres.me>
2025-05-13 08:05:23 +0000

    Update CommandPrompt.tsx * Remove bug where the useState setter is passed a function that results in [Object object] being added to the input

--- src/components/Chat/ChatContent/Message/CommandPrompt/CommandPrompt.tsx
+++ src/components/Chat/ChatContent/Message/CommandPrompt/CommandPrompt.tsx
@@ -70,7 +70,7 @@ const CommandPrompt = ({
             <li
               className='px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer text-start w-full'
               onClick={() => {
-                _setContent((prev) => [{type: 'text', text: prev + cp.prompt}, ...prev.slice(1)]);
+                _setContent([{type: 'text', text: cp.prompt}]);
                 setDropDown(false);
               }}
               key={cp.id}
